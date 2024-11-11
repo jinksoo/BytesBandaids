a2beade1cd872961915c48cf0c5e96854dc92d85
Laura Chen <laurachen@Lauras-Macbook16-2.local>
2024-11-10 17:25:52 -0800

    MTS

--- parseconvos.py
+++ parseconvos.py
@@ -2,29 +2,49 @@ import pandas as pd
 import re
 import json
 
-# Load the JSONL file into a DataFrame
-data = []
+import pandas as pd
+import re
+import json
 
-# Specify input file here
-dataset_file = "/Users/laurachen/Documents/Github/clinical_visit_note_summarization_corpus/aci_train.jsonl"
 
-with open(dataset_file, "r") as file:
-    for line in file:
-        data.append(json.loads(line))
-df = pd.DataFrame(data)
+
+def load_data(dataset_file):
+    # Load the JSONL file into a DataFrame
+    data = []
+    if dataset_file.endswith('.jsonl'):
+        with open(dataset_file, "r") as file:
+            for line in file:
+                data.append(json.loads(line))
+        df = pd.DataFrame(data)
+    elif dataset_file.endswith('.csv'):
+            df = pd.read_csv(dataset_file)
+    else:
+        raise ValueError("Unsupported file format. Please use .jsonl or .csv.")
+    return df
 
 # Define functions to extract dialogues
 def extract_doctor_dialogue(text):
-    matches = re.findall(r"\[doctor\](.*?)(?=\[patient\]|\Z)", text, re.DOTALL)
+    if 'aci' in dataset_file:
+        matches = re.findall(r"\[doctor\](.*?)(?=\[patient\]|\Z)", text, re.DOTALL)
+       
+    elif 'MTS' in dataset_file:
+        matches = re.findall(r"Doctor:\s*(.*?)(?=Patient:|\Z)", text, re.DOTALL)
+
     return ' '.join(matches).strip() if matches else None
 
 def extract_patient_dialogue(text):
-    matches =re.findall(r"\[patient\](.*?)(?=\[doctor\]|\Z)", text, re.DOTALL)
+    if 'aci' in dataset_file:
+        matches = re.findall(r"\[patient\](.*?)(?=\[doctor\]|\Z)", text, re.DOTALL)
+    elif 'MTS' in dataset_file:
+        matches = re.findall(r"Patient:\s*(.*?)(?=Doctor:|\Z)", text, re.DOTALL)  
     return ' '.join(matches).strip() if matches else None
+
+# Specify input file here
+dataset_file = "data/mts-dialog/MTS_Dataset_TrainingSet.csv"
+df = load_data(dataset_file)
 
 # Apply the functions to extract dialogues and add as new columns
 df['doctor_dialogue'] = df['dialogue'].apply(extract_doctor_dialogue)
 df['patient_dialogue'] = df['dialogue'].apply(extract_patient_dialogue)
 
-# Display the result
-print(df[['doctor_dialogue', 'patient_dialogue']].head())
+print(df['patient_dialogue'])
